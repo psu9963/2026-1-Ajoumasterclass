c6ad0a26946e0620fa80dbda67da6205172f426d
박동원 <bagdong-won@bagdong-won-ui-MacBookAir.local>
2026-04-29 17:36:44 +0900

    대시보드 화면

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from views.dashboard import dashboard_bp
 
 app = Flask(__name__)
+app.secret_key = 'dev-secret-key'
+
+app.register_blueprint(dashboard_bp) 
 
 @app.route('/')
 def home():
