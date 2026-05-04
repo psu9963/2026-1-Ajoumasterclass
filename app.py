94a5459c47aa217a90dc6d7a4470774066384036
박동원 <bagdong-won@bagdong-won-ui-MacBookAir.local>
2026-05-04 16:08:32 +0900

    AI학습계획

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from views.login import login_bp
 from models import db
 from views.dashboard import dashboard_bp
 from views.study_history import study_history_bp
+from views.ai_plan import ai_plan_bp
 
 
 # 1. 🌟 가장 먼저 플라스크 앱(app)을 만들어 줍니다! 🌟
 app = Flask(__name__)
 app.secret_key = 'dev-secret-key'
-
-app.register_blueprint(dashboard_bp) 
 
 # 2. 앱이 만들어진 후에 데이터베이스 설정을 해줍니다.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -25,6 +24,8 @@ with app.app_context():
 # 블루프린트 등록
 app.register_blueprint(login_bp)
 app.register_blueprint(study_history_bp)
+app.register_blueprint(ai_plan_bp)
+app.register_blueprint(dashboard_bp)
 
 
 @app.route('/')
